54133e3b70cf05f301bd5dcd34a93ef33f376592
max <skymax1998@gmail.com>
2018-12-12 01:30:22 +0300

    well async reader completed! Just like in msdn, hehe

--- TestGeneratorLib/Utils/FileWriter.cs
+++ TestGeneratorLib/Utils/FileWriter.cs
@@ -1,7 +1,25 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
 namespace TestGeneratorLib.Utils
 {
     public class FileWriter
     {
-        
+        public async Task<string> ReadAsync(string filepath)
+        {
+            try
+            {
+                using (StreamReader streamReader = new StreamReader(filepath))
+                {
+                    return await streamReader.ReadToEndAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went terribly wrong, the file could not be read.");
+                return ""; //wut?
+            }
+        }
     }
 }
